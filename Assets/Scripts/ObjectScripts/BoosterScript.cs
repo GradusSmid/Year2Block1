a05118e3bd9b9127ebf5f991b24c27d18e9b8170
Landerack <Robinvermeulenbedum@gmail.com>
2019-10-09 16:14:05 +0200

    Unfinnished Booster
    Making it with active or passive states, so the animation/particles can also be played or stopped upon entering the field. Haven't yet figured out exactly what the syntax is for playing such things, so will add that when I figure it out.

---
+++ Assets/Scripts/ObjectScripts/BoosterScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleAccess : MonoBehaviour {
+    private bool touchingBooster = false;
+
+    public GameObject Player;
+    // Update is called once per frame
+    void Update()
+    {
+
+            //check for player's collision with game object tagged Booster
+            if (touchingBooster)
+            {
+                
+            }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Booster")
+        {
+            touchingBooster = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Booster")
+        {
+            touchingBooster = false;
+        }
+    }
+}
