4f92aac527a5a88cd70ef953aedea8eb5fb81686
Landerack <Robinvermeulenbedum@gmail.com>
2019-10-15 20:32:34 +0200

    Committing to pull

--- Assets/Scripts/CameraController/RegularCamera/AnchorMovement.cs
+++ Assets/Scripts/CameraController/RegularCamera/AnchorMovement.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class AnchorMovement : MonoBehaviour
 {
     //identify every player
-
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject Player3;
+    public GameObject Player4;
 
 
     // Update is called once per frame
     void FixedUpdate()
     {
         //Every player's X value is added up then defided by full number of players. This is now the Camera Anchor's Y value.
+        
         //Every player's Y value is added up then defided by full number of players. This is now the Camera Anchor's Y value.
     }
 }
