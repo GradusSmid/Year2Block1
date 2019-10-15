7bd2a533633068247c0cfc75c505296d7481f499
Landerack <Robinvermeulenbedum@gmail.com>
2019-10-15 15:22:33 +0200

    Updating things. Camera temp disabled btw
    There are a ton of changes. I have temporarily dissabled the camera :) no worries.
    It'll soon be back up with new running code. The blueprint is written, will try and fix it entirely. The deathblock is already fixed. I was also thinking about adding permanent bodies on the place of the dead. actually, I would like to add guts and a skull :P that then permanently stay.

---
+++ Assets/Scripts/CameraController/RegularCamera/AnchorMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnchorMovement : MonoBehaviour
+{
+    //identify every player
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Every player's X value is added up then defided by full number of players. This is now the Camera Anchor's Y value.
+        //Every player's Y value is added up then defided by full number of players. This is now the Camera Anchor's Y value.
+    }
+}
+
+//Important. The next step is to attach the springjoint with the camera to the anchor. this way it will rubberband.
