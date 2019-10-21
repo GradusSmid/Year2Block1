50f1c175840bd74c85be4b81d30cbda658c188c8
Gradus Smid <33928046+GradusSmid@users.noreply.github.com>
2019-10-21 16:00:35 +0200

    ..

---
+++ Assets/Scripts/ObjectScripts/Booster/BoosterStopper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoosterStopper : MonoBehaviour
+{
+    Vector2 velocity;
+    void OnTriggerStay2D(Collider2D other)//other here is where we check the thing that colides.
+    {
+        if ((other.gameObject.tag == "Player") || (other.gameObject.tag == "Item"))//if the other is tagged with player, do the following.
+        {
+            Debug.Log("working");
+
+            //boost player or object up
+            velocity = other.GetComponent<Rigidbody2D>().velocity;
+            velocity = new Vector2 (0, 0);
+            
+        }
+    }
+}
