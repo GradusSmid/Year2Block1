2ea78c07a091179cef36e6231e7b7e5ae7caa7a9
Tilly-World <t.m.broad@st.hanze.nl>
2019-11-04 22:22:37 +0100

    asset size change, learn coding anf tile fixing

---
+++ Assets/Scripts/ChangeToPlay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeToPlay : MonoBehaviour
+{
+  private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+          
+           StartCoroutine("FinishLine");
+        }
+    }
+
+    IEnumerator FinishLine()
+    {
+        yield return new WaitForSeconds(1.6f);
+        SceneManager.LoadScene(1);
+        yield return null;
+    }
+    }
