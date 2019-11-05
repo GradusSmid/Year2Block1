1292216c051f77cf17725b2a63692a769512628d
Gradus Smid <33928046+GradusSmid@users.noreply.github.com>
2019-11-06 00:03:45 +0100

    ahh

--- Assets/Scripts/ObjectScripts/MovingPlatform.cs
+++ Assets/Scripts/ObjectScripts/MovingPlatform.cs
@@ -7,22 +7,22 @@ public class MovingPlatform : MonoBehaviour
     int Check = 0;
     void Update()
     {
-
+        if (Check == 0)
         {
             while ((Check >= 0) && (Check != 500))
             {
                 transform.Translate(Vector2.right * Time.deltaTime);
                 Check++;
-                if (Check == 500)
-            {
-                    while ((Check != 0)&&(Check <= 500))
-                    {
-                        transform.Translate(Vector2.left * Time.deltaTime);
-                        Check++;
-                    }
-                }
             }
 
+        }
+        if (Check == 500)
+        {
+            while ((Check <= 500) && (Check != 0))
+            {
+                transform.Translate(Vector2.left * Time.deltaTime);
+                Check--;
+            }
         }
     }
 }
