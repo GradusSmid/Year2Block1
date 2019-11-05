3d575016825191eea53152cdb576941711769844
Gradus Smid <33928046+GradusSmid@users.noreply.github.com>
2019-11-05 17:25:08 +0100

    and again

---
+++ Assets/Scripts/ObjectScripts/MovingPlatform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    int Check = 0;
+    void Update()
+    {
+
+        {
+            while ((Check >= 0) && (Check != 500))
+            {
+                transform.Translate(Vector2.right * Time.deltaTime);
+                Check++;
+                if (Check == 500)
+            {
+                    while ((Check != 0)&&(Check <= 500))
+                    {
+                        transform.Translate(Vector2.left * Time.deltaTime);
+                        Check++;
+                    }
+                }
+            }
+
+        }
+    }
+}
