3b1fe1243d578e3f140f634dfac147de6d4652a4
Gradus Smid <33928046+GradusSmid@users.noreply.github.com>
2019-11-05 10:17:15 +0100

    Clean up, changed the boundary box and grappling hook improvement
    yeye

--- Assets/Scripts/Player1 Scripts/Movement.cs
+++ Assets/Scripts/Player1 Scripts/Movement.cs
@@ -92,11 +92,14 @@ public class Movement : MonoBehaviour
         }
         else
             isGrounded = false;
-        
+        // Boudning
+        Vector3 minScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
 
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minScreenBounds.x + 1, maxScreenBounds.x - 1), Mathf.Clamp(transform.position.y, minScreenBounds.y + 1, maxScreenBounds.y - 3), transform.position.z);
 
         //Arm movement
-       
+
         arm.transform.localPosition = new Vector3(Input.GetAxis("HorizontalRStick"), Input.GetAxis("VerticalRStick"), 0).normalized;
 
         // Rotation of arm
